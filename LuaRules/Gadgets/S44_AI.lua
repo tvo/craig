1aef4fbf4d5e7a959780bf04ad1d18fdd8a6b56e
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-02-08 14:05:01 +0100

    Initial framework, based on KPAI.
    Adds "/luarules s44ai [0|1]" chat command to change debug mode.
    Debug mode is by default on currently.

---
+++ LuaRules/Gadgets/S44_AI.lua
@@ -0,0 +1,109 @@
+-- Author: Tobi Vollebregt
+-- Slightly based on the Kernel Panic AI by KDR_11k (David Becker) and zwzsg.
+
+-- In-game, type /luarules s44ai in the console to toggle the ai debug messages
+
+function gadget:GetInfo()
+	return {
+		name = "Spring: 1944 AI",
+		desc = "An AI for Spring: 1944",
+		author = "Tobi Vollebregt",
+		date = "2009-02-08",
+		license = "GNU General Public License",
+		layer = 82,
+		enabled = true
+	}
+end
+
+
+
+local S44_AI_Debug_Mode = 1 -- Must be 0 or 1
+
+if (gadgetHandler:IsSyncedCode()) then
+
+--SYNCED
+
+local teamData={}
+
+
+local function ChangeAIDebugVerbosity(cmd,line,words,player)
+	local lvl = tonumber(words[1])
+	if lvl then
+		S44_AI_Debug_Mode = lvl
+		Spring.Echo("S44 LUA AI debug verbosity set to " .. S44_AI_Debug_Mode)
+	else
+		if S44_AI_Debug_Mode > 0 then
+			S44_AI_Debug_Mode = 0
+		else
+			S44_AI_Debug_Mode = 1
+		end
+		Spring.Echo("S44 LUA AI debug verbosity toggled to " .. S44_AI_Debug_Mode)
+	end
+	return true
+end
+
+
+local function SetupCmdChangeAIDebugVerbosity()
+	local cmd,func,help
+	cmd  = "s44ai"
+	func = ChangeAIDebugVerbosity
+	help = " [0|1]: make the S44 LUA AI shut up or fill your infolog"
+	gadgetHandler:AddChatAction(cmd,func,help)
+	Script.AddActionFallback(cmd .. ' ',help)
+end
+
+
+local function DebugMessage(t,message)
+	if S44_AI_Debug_Mode > 0 then
+		Spring.Echo("S44AI: Team[" .. t .. "] " .. message)
+	end
+end
+
+
+function gadget:Initialize()
+	SetupCmdChangeAIDebugVerbosity()
+end
+
+
+local function RemoveSelfIfNoTeam()
+	local AIcount=0
+	for t,td in pairs(teamData) do
+		AIcount = AIcount + 1
+	end
+	if (AIcount == 0) then -- #teamData is 0 even when there are teams, and teamData=={} is untrue even when teamData={}
+		DebugMessage(gadget:GetInfo().name, "removing self")
+		gadgetHandler:RemoveGadget()
+	end
+end
+
+
+function gadget:GameStart()
+	DebugMessage(gadget:GetInfo().name, "GameStart!")
+
+	-- Initialise AI for all team that are set to use it
+	for _,t in ipairs(Spring.GetTeamList()) do
+		local _,_,_,ai,side = Spring.GetTeamInfo(t)
+		if Spring.GetTeamLuaAI(t) == gadget:GetInfo().name then
+			DebugMessage(t, "assigned to " .. gadget:GetInfo().name)
+			local _,_,_,_,_,at = Spring.GetTeamInfo(t)
+			teamData[t] = {
+				allyTeam = at,
+			}
+		end
+	end
+	-- RemoveSelfIfNoTeam() -- Somehow gadgetHandler:RemoveGadget() remove other gadgets when executed at GameStart stage. Moved to GameFrame
+end
+
+
+function gadget:GameFrame(f)
+	-- AI update
+	if f % 128 < .1 then
+		RemoveSelfIfNoTeam()
+	end
+end
+
+else
+
+--UNSYNCED
+
+end
