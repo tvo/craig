a546eba0714b726f1c720599ad5a1ff526243da3
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-02-08 12:56:29 +0100

    Start as mutator of S44 SVN.

---
+++ modinfo.lua
@@ -0,0 +1,17 @@
+-- Author: Tobi Vollebregt
+
+local modinfo = {
+	name = "Spring: 1944 SVN + AI",
+	shortName = "S44",
+	game = "Spring 1944",
+	shortGame = "S44",
+	mutator = "AI for Spring: 1944",
+	description = "AI for Spring: 1944",
+	url = "http://www.spring1944.com",
+	modtype = "1",
+	depend = {
+		"Spring: 1944 SVN"
+	},
+}
+
+return modinfo
