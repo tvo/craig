6869157d75c6412ef8631ce7093894b12738ba7f
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-02-14 12:03:45 +0100

    Helper script to merge back changes made in the S44 SVN.

---
+++ merge_from_svn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author: Tobi Vollebregt
+# License: GNU General Public License v2
+#
+# Copies SVN version of all files currently in git repo,
+# to the git repo.  "../S44LiteRelease.sdd/" must exist!
+#
+
+if ! git diff --quiet; then
+	echo "Local modifications present, aborted"
+	exit 1
+fi
+
+# '*' instead of '.' to exclude .git
+for dst in `find * -type f ! -name modinfo.lua`; do
+	src="../S44LiteRelease.sdd/$dst"
+	[ -f "$src" ] && cp "$src" "$dst" && dos2unix "$dst"
+done
