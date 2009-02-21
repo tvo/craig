b068d52ea1f74d9beadb8d8b77a2f452a41ebaf1
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-02-21 01:15:33 +0100

    Depend on Konstantin instead of Spring: 1944 SVN.

--- modinfo.lua
+++ modinfo.lua
@@ -1,11 +1,8 @@
 -- Author: Tobi Vollebregt
 -- License: GNU General Public License v2
 
--- NOTE: this modinfo.lua is only used for developing,
--- the released modinfo.lua is embedded in make_release.sh
-
 local modinfo = {
-	name = "Spring: 1944 SVN + AI",
+	name = "Spring: 1944 Konstantin + C.R.A.I.G. (git)",
 	shortName = "S44",
 	game = "Spring 1944",
 	shortGame = "S44",
@@ -14,7 +11,7 @@ local modinfo = {
 	url = "http://www.spring1944.com",
 	modtype = "1",
 	depend = {
-		"Spring: 1944 SVN"
+		"Spring: 1944 Operation Konstantin (v0.92)"
 	},
 }
 
