9df0a2fe758b19e0b3714d43b2aff6725e2f5093
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-02-28 16:14:35 +0100

    Update release script for IW.

--- tools/make_release.sh
+++ tools/make_release.sh
@@ -16,17 +16,17 @@ cat > .tmp/modinfo.lua << EOD
 -- License: GNU General Public License v2
 
 local modinfo = {
-	name = "Spring: 1944 Konstantin + C.R.A.I.G. (v2.3)",
-	shortName = "S44",
-	game = "Spring 1944",
-	shortGame = "S44",
-	mutator = "AI for Spring: 1944",
-	description = "AI for Spring: 1944",
-	url = "http://www.spring1944.com",
+	name = "Imperial Winter 1.5.051 beta + C.R.A.I.G. (v2.3 alpha)",
+	shortname = "SWIW",
+	game = "Star Wars: Imperial Winter",
+	shortgame = "SWIW",
+	mutator = "AI for SWIW",
+	description = "AI for Spring: 1944 hastily bodged into SWIW",
+	url = "www.meatspin.com",
 	modtype = "1",
 	depend = {
-		"Spring: 1944 Operation Konstantin (v0.92)"
-	},
+		"sws.v051.sdz",
+	}
 }
 
 return modinfo
