93d30855e89563a1d6becd8f7fbac73fca405812
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-03-04 21:19:09 +0100

    Changelog update and version bump to 3.0

--- tools/make_release.sh
+++ tools/make_release.sh
@@ -16,7 +16,7 @@ cat > .tmp/modinfo.lua << EOD
 -- License: GNU General Public License v2
 
 local modinfo = {
-	name = "Spring: 1944 Konstantin + C.R.A.I.G. (v2.3)",
+	name = "Spring: 1944 Konstantin + C.R.A.I.G. (v3.0)",
 	shortName = "S44",
 	game = "Spring 1944",
 	shortGame = "S44",
