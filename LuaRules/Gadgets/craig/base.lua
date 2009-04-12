c4c532a4a44eb6aca4cee443609ef3b2c226de26
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-04-12 17:30:19 +0200

    Fix infinite loop when there was nothing to build anymore.
    (Due to unit limits.)

--- LuaRules/Gadgets/craig/base.lua
+++ LuaRules/Gadgets/craig/base.lua
@@ -69,12 +69,15 @@ local function BuildBase()
 	-- nothing to do if something is still being build
 	if currentBuildDefID then return end
 
+	-- fix for infinite loop if baseBuildIndex <= 0
+	local count, maxcount = 1, #baseBuildOrder
 	local unitDefID
 	local newIndex = baseBuildIndex
 	repeat
-		newIndex = (newIndex % #baseBuildOrder) + 1
+		newIndex = (newIndex % maxcount) + 1
 		unitDefID = baseBuildOrder[newIndex]
-	until (newIndex == baseBuildIndex) or
+		count = count + 1
+	until (count > maxcount) or
 		-- check if Spring would block this build (unit restriction)
 		((Spring.GetTeamUnitDefCount(myTeamID, unitDefID) or 0) < UnitDefs[unitDefID].maxThisUnit and
 		-- check if some part of the AI would block this build
